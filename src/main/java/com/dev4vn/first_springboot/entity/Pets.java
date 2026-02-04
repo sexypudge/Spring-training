52561884c64544a9f0b24d97c67dca34614da48d
thanhdepzy <T01659854125@gmail.com>
2026-02-04 22:28:53 +0700

    Spring JPA

---
+++ src/main/java/com/dev4vn/first_springboot/entity/Pets.java
@@ -0,0 +1,37 @@
+package com.dev4vn.first_springboot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pets")
+public class Pets {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
